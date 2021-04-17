868a56d69a4b55e0d29beae2e110d1f0beb461c4
mehmet ali <alimehmet1@windowslive.com>
2021-04-17 18:56:01 +0300

    17042021

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Dictionary<K,V>
+    {
+        K[] _key;
+        V[] _value;
+        public Dictionary()
+        {
+            _key = new K[0];
+            _value = new V[0];
+        }
+        public void Add(K key,V value)
+        {
+            K[] _tempKey = _key;
+            V[] _tempValue = _value;
+
+            _key = new K[_key.Length + 1];
+            _value = new V[_value.Length + 1];
+
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1,"Mehmet");
+            dictionary.Add(2, "Salih");
+            dictionary.Add(3, "Cemile");
+            dictionary.Add(4, "Murat");
+
+        }
+    }
+}
